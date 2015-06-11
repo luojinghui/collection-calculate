3f42c43b97fe852fa078e8ddff43b69c45ea69ed
luojinghui <939035243@qq.com>
2015-06-11 14:39:54 +0800

    get_intersection.js with intersection function in prolodash

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,18 +1,8 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_intersection(collection_a, collection_b) {
-    return _.intersection(collection_b,collection_a);
-
+    return _(collection_b).intersection(_(collection_a)).value();
 }
 
-
-    // var result = [];
-    // for(var i=0; i<collection_b.length; i++) {
-    //     for(var j=0; j<collection_a.length; j++) {
-    //         if(collection_b[i] === collection_a[j]) {
-    //             result.push(collection_b[i]);
-    //         }
-    //     }
-    // }
 module.exports = get_intersection;
