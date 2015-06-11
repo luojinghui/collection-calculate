803467ab01fb4fc03fd8665be8d4e0b236d92639
luojinghui <939035243@qq.com>
2015-06-11 14:51:17 +0800

    get_letter_interval.js with range adn map function in prototype chain

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,30 +1,9 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_letter_interval(number_a, number_b) {
-    var array = [];
-    var result = [];
-
-    array = _.range(number_a,number_b);
-
-    return _.map(array,function(n) {
-        return (_.num_change_letter(n));
-    });
+    return _(number_a).range(number_b).map(function(n) {
+        return String.fromCharCode(n + 96);
+    }).value();
 }
-
 module.exports = get_letter_interval;
-
-/*
-    var array = [];
-
-    if(number_a <= number_b) {
-    for(var i=number_a; i<=number_b; i++){
-        array.push(String.fromCharCode(i+96));
-        }
-    }else {
-        for(var j=number_a; j>=number_b; j--){
-            array.push(String.fromCharCode(j+96));
-            }
-    }
-    return array;
-    */
