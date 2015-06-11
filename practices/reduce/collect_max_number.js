cf942e9814086a117a107cfcf6d69eda327a9d14
luojinghui <939035243@qq.com>
2015-06-11 13:14:39 +0800

    use prolodash.js soveld collect_max_number.js

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,26 +1,9 @@
 'use strict';
+var _ = require('../chain/prolodash.js');
 
 function collect_max_number(collection) {
-
-    var _ = require('../lodash/mylodash.js');
-    return _.reduce(collection,function(a,b) {
+    return _(collection).reduce(function(a,b) {
         return Math.max(a,b);
-    });
+    }).value();
 }
 module.exports = collect_max_number;
-
-/*
-var result;
-var it = 0;
-
-if(collection.length === 1) {
-    result = collection[0];
-}else {
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[it] < collection[i]) {
-            it = i;
-        }
-}
-}
-return collection[it];
-*/
