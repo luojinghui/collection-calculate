c61854f8ace70361bdd0513fa10ab63220be7601
luojinghui <939035243@qq.com>
2015-06-11 13:21:58 +0800

    use prolodash.js soveld collect_last_element

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,6 +1,10 @@
 'use strict';
-function collect_all_even(collection) {
+var _ = require('../chain/prolodash.js');
 
+function collect_all_even(collection) {
+    return _(collection).filter(function(n) {
+        return n % 2 === 0;
+    }).value();
 }
 
 module.exports = collect_all_even;
