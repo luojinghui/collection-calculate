50c05b973e99e436ebb3a7cc5bebd51ff8c6781f
luojinghui <939035243@qq.com>
2015-06-11 13:56:32 +0800

    use prolodash.js solve get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,24 +1,8 @@
 
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_integer_interval(number_a, number_b) {
-    return _.range(number_a,number_b);
-
-//     var a = [];
-//
-//     if(number_a <= number_b) {
-// //        for(var i=number_a; i<=number_b; i++) {
-// //            a.push(i);
-//         _.foreach(collection_a,function(n,i) {
-//             a.push(i);
-//         });
-//         }
-//     } else {
-//         _.foreach(collection_b,function(n,i) {
-//             a.push(j);
-//         });
-//     }
-//     return a;
+    return _(number_a).range(number_b).value();
 }
 module.exports = get_integer_interval;
