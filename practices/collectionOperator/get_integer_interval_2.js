361950459ca19fd7e7255adfc8fb06130dacdb8a
luojinghui <939035243@qq.com>
2015-06-11 14:11:33 +0800

    alter two lines change one line

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,12 +2,10 @@
 var _ = require('../chain/prolodash.js');
 
 function get_integer_interval_2(number_a, number_b) {
-    var array = _(number_a).range(number_b).value();
-    var result = _(array).filter(function(n) {
+    return _(number_a).range(number_b).filter(function(n) {
         return n % 2 === 0;
     }).value();
 
-    return result;
 }
 
 module.exports = get_integer_interval_2;
