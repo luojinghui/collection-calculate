84da47a0daea6875827633dea54685a801e86028
luojinghui <939035243@qq.com>
2015-06-11 15:12:43 +0800

    get_letter_interval_2.js with range and map function in prototype chain

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,12 +1,10 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_letter_interval_2(number_a, number_b) {
-    var array = _.range(number_a,number_b);
-
-    return _.map(array,function(n) {
-        return _.num_change_letter(n);
-    });
+    return _(number_a).range(number_b).map(function(n) {
+        return _().num_change_letter(n);
+    }).value();
 }
 
 module.exports = get_letter_interval_2;
