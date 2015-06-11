422aea8a18a0fb9962b998f8c52e5c17b2288e75
luojinghui <939035243@qq.com>
2015-06-11 13:26:55 +0800

    use prolodash.js solve collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,10 +1,10 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function collect_last_element(collection) {
-    return _.reduce(collection,function(a,b) {
+    return _(collection).reduce(function(a,b) {
         return b;
-    })
+    }).value();
 }
 
 module.exports = collect_last_element;
