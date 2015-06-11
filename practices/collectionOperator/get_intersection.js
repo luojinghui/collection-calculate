08f68fab41193647085b05a421f42f3c24fb0862
luojinghui <939035243@qq.com>
2015-06-11 15:22:14 +0800

    get_intersection.js with range and map function in prototype chain

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,7 +2,7 @@
 var _ = require('../chain/prolodash.js');
 
 function get_intersection(collection_a, collection_b) {
-    return _(collection_b).intersection(_(collection_a)).value();
+    return _(collection_b).intersection(collection_a).value();
 }
 
 module.exports = get_intersection;
