f8573929e933be1fbbcd8d091c7dd0d0d34b2ed4
luojinghui <939035243@qq.com>
2015-06-11 15:44:07 +0800

    get_union.js with concat and no_repeat function in prototype chain

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,36 +1,7 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_union(collection_a, collection_b) {
-    var array = _.concat(collection_a,collection_b);
-    
-    return _.no_repeat(array);
+    return _(collection_a).concat(collection_b).no_repeat().value();
 }
 module.exports = get_union;
-
-
-/*
-function grouping_count(collection) {
-  //
-  // //在这里写入代码
-  // var _ = require('../lodash');
-  // return _.countBy(collection);
-  //collection.sort();
-  var temp = {};
-
-  for(var i = 0;i < collection.length;i++) {
-    //   temp[collection[i]]=temp[collection[i]]||0;
-    //   temp[collection[i]]++;
-      if(!! temp[collection[i]]) {
-          temp[collection[i]]++;
-      }else{
-          temp[collection[i]]=0;
-          temp[collection[i]]++;
-      }
-
-  }
-  return temp;
-}
-
-module.exports = grouping_count;
-*/
