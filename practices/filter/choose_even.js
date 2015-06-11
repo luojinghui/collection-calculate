b5b2dc359b80cb5d044b1cc543c926afb34c8273
luojinghui <939035243@qq.com>
2015-06-11 13:13:48 +0800

    use prolodash.js soveld choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,17 +1,12 @@
 'use strict';
+var _ = require('../chain/prolodash.js');
 
 function choose_even(collection) {
-    //在这里写入代码
-//    var _ = require('../lodash/array');
-//    var evens = _.remove(collection, function(n) {
-//        return n % 2 == 0;
-//    });
-//    return evens;
-
-    var _ = require('../lodash/mylodash.js');
-    return _.filter(collection,function(n) {
-        return n % 2 === 0;
-    });
+    return _(collection).filter(function(n,i) {
+        if(n % 2 === 0) {
+            return true;
+            }
+        }).value();
 }
 
 module.exports = choose_even;
