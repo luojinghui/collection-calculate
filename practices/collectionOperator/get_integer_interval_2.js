8818cb26328d9cf37229b54682ad91f1082ce64e
luojinghui <939035243@qq.com>
2015-06-11 14:07:03 +0800

    use prolodash.js solve get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,48 +1,13 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function get_integer_interval_2(number_a, number_b) {
-    var array = _.range(number_a,number_b);
-    var result = _.map(array,function(n) {
+    var array = _(number_a).range(number_b).value();
+    var result = _(array).filter(function(n) {
         return n % 2 === 0;
-    });
+    }).value();
+
     return result;
 }
 
-
-/*
-//在这里写入代码
-var array = [];
-
-if(number_a != number_b) {
-    num_intervel_up(array,number_a,number_b);
-}else {
-    same_num(array,number_a,number_b);
-}
-return array;
-
-function num_intervel_up(array,number_a,number_b) {
-    if(number_a < number_b) {
-        for(var i=number_a; i<=number_b; i++) {
-            var even_number = 0;
-
-            if(i % 2 === 0){
-                array.push(i);
-            }
-        }
-    }else {
-        for(var j=number_a; j>number_b; j--){
-            if(j % 2 === 0){
-                array.push(j);
-            }
-        }
-    }
-}
-
-function same_num(array,number_a,number_b) {
-    if(number_a === number_b && number_a % 2 === 0) {
-        array.push(number_a);
-    }
-}
-*/
 module.exports = get_integer_interval_2;
