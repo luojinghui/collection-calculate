ae6d342aea8b08dc7a972d8ad674740e293759be
luojinghui <939035243@qq.com>
2015-06-12 13:05:43 +0800

    number_map_to_word_over_26 with map and num_change_letter function in prototype chain

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,13 +1,10 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 var number_map_to_word_over_26 = function(collection){
-    var letter = [];
-
-    _.foreach(collection,function(n) {
-        letter.push(_.num_change_letter(n));
-    });
-    return letter;
-};
+    return _(collection).map(function(n) {
+        return _().num_change_letter(n);
+    }).value();
+}
 
 module.exports = number_map_to_word_over_26;
