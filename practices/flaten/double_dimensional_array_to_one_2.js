3b8a97baf21f0d1860e2674e076e3a179a57c42b
luojinghui <939035243@qq.com>
2015-06-12 12:51:18 +0800

    double_dimesional_array_to_noe_2 with each and no_repeat function in prorotype chain

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,15 +1,22 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 function double_to_one(collection) {
     var result = [];
 
-    _.foreach(collection,function(n) {
-        for(var j = 0; j < n.length; j++) {
-            result.push(n[j]);
-        }
+    // _.foreach(collection,function(n) {
+    //     for(var j = 0; j < n.length; j++) {
+    //         result.push(n[j]);
+    //     }
+    // });
+    // return _.no_repeat(result);
+
+    _(collection).each(function(n) {
+        _(n).each(function(m) {
+            result.push(m);
+        })
     });
-    return _.no_repeat(result);
+    return _(result).no_repeat().value();
 }
 
 module.exports = double_to_one;
