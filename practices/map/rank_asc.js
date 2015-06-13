66a3eae00442d9d9710246ee03bd842d92ef6545
luojinghui <939035243@qq.com>
2015-06-13 20:53:32 +0800

    map/rang_asc.js with sort function in prototype chain

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,32 +1,10 @@
 'use strict';
-var _ = require('../lodash/mylodash.js');
+var _ = require('../chain/prolodash.js');
 
 var rank_asc = function(collection){
-    return _.sort_array(collection,function(a,b) {
-        return a > b;
-    });
+    return _(collection).sort(function(a,b) {
+        return a < b;
+    }).value();
 
-};
-
+}
 module.exports = rank_asc;
-
-/*
- return collection.sort(sortNumber);
- var array_sort = [];
- if(collection.length === 1) {
-     return collection[0];
- } else {
- for (var i = 0; i < collection.length; i ++) {
-     var temp = 0;
-
-     for(var j = i + 1; j < collection.length; j ++) {
-         if (collection[i] < collection[j]) {
-             temp = collection[j];
-             collection[j] = collection[i];
-             collection[i] = temp;
-             }
-         }
-     }
- }
- return collection;
- */
