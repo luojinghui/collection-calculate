90f5360aef714cbde02d2cab8396efb9ca7f4c0f
luojinghui <939035243@qq.com>
2015-06-14 19:05:19 +0800

    even_asc_odd_desc.js with sort and each function in prototype chain

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,24 +1,25 @@
 'use strict';
-var _ = require('../../lodash/mylodash.js');
+var _ = require('../../chain/prolodash.js');
 
 var even_asc_odd_desc = function(collection){
+    var collection_a = _(collection).sort(function(a,b) {
+        return a > b;
+    }).value();
+
     var array = [];
     var array_2 = [];
-    var collection_a = _.sort_array(collection,function(a,b) {
-        return a < b;
-    });
 
-    _.foreach(collection_a,function(n) {
+    _(collection_a).each(function(n) {
         if(n % 2 === 0) {
             array.push(n);
         }
-    });
+    })
 
     for (var a = collection_a.length - 1; a >= 0; a --) {
         if(collection_a[a] % 2 != 0) {
             array_2.push(collection_a[a]);
         }
     }
-    return _.concat(array,array_2);
+    return _(array).concat(array_2).value();
 };
 module.exports = even_asc_odd_desc;
