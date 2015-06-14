3ad34a95a40bcbb9bd3627a11ea4246b3c19d3a4
luojinghui <939035243@qq.com>
2015-06-14 16:19:31 +0800

    practice_3 with filter map and reduce function in prototype chain

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,26 +1,14 @@
 'use strict';
-var _ = require('../../lodash/mylodash.js');
+var _ = require('../../chain/prolodash.js');
 
 function hybrid_operation_to_uneven(collection) {
-    // var sum = 0 ;
-    // for (var i = 0; i < collection.length; i++) {
-    //     if(collection[i] % 2 != 0 ) {
-    //         sum += (collection[i] * 3 + 5);
-    //     }
-    // }
-    // return sum;
-
-    var sum = 0;
-    var array = _.filter(collection,function(n) {
+    return _(collection).filter(function(n) {
         return n % 2 != 0;
-    });
-
-    var array_odd = _.map(array,function(n) {
-        return (n * 3 + 5);
-    });
-    return _.reduce(array_odd,function(a,b) {
-        return a + b ;
-    });
+    }).map(function(n) {
+        return n * 3 +5;
+    }).reduce(function(a,b) {
+        return a + b;
+    }).value();
 }
 
 module.exports = hybrid_operation_to_uneven;
