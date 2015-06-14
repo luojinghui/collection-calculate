1e7db5a0bf2a8c1f56ca302b4eb850896742fb49
luojinghui <939035243@qq.com>
2015-06-14 12:04:31 +0800

    compute_average.js with reduce function in prototype chain

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,21 +1,18 @@
 'use strict';
-var _ =require('../lodash/mylodash.js');
+var _ =require('../chain/prolodash.js');
 
 function compute_average(collection) {
-    //在这里写入代码
-    // var num = 0;
-    // var result = 0;
-    // for (var i = 0; i < collection.length; i++) {
-    //     num += collection[i];
-    // }
-    // result = num / collection.length;
-    // return result;
-    var sum;
-
-    sum = _.reduce(collection,function(a,b) {
+    var sum = _(collection).reduce(function(a,b) {
         return a + b;
-    });
-    return (sum / collection.length);
+    }).value();
+    return sum / collection.length;
+    //
+    // var sum;
+    //
+    // sum = _.reduce(collection,function(a,b) {
+    //     return a + b;
+    // });
+    // return (sum / collection.length);
 }
 
 module.exports = compute_average;
