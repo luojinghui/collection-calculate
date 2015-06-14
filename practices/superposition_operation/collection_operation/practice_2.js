4856b5aaf8e2dfd66a7d3d35ebd41b9377f35f7e
luojinghui <939035243@qq.com>
2015-06-14 16:16:06 +0800

    practice_2 .js with filter map and value function in prototype chain

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,23 +1,12 @@
 'use strict';
-var _ = require('../../lodash/mylodash.js');
+var _ = require('../../chain/prolodash.js');
 
 function hybrid_operation_to_uneven(collection) {
-    // var sum = [] ;
-    // for (var i = 0; i < collection.length; i++) {
-    //     if(collection[i] % 2 != 0 ) {
-    //         sum[sum.length] = (collection[i] * 3 + 2);
-    //     }
-    // }
-    // return sum;
-
-    var array_odd = _.filter(collection,function(n) {
-        return (n % 2 != 0);
-    });
-
-    var array = _.map(array_odd,function(n) {
-        return (n * 3 + 2);
-    });
-    return array;
+    return _(collection).filter(function(n) {
+        return n % 2 != 0;
+    }).map(function(n) {
+        return n * 3 + 2;
+    }).value();
 }
 
 module.exports = hybrid_operation_to_uneven;
