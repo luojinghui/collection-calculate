2576846313b9f6cf6df5783cf250666d58fce622
luojinghui <939035243@qq.com>
2015-06-14 12:33:54 +0800

    find_element_first_subscript.js with each function in prototype chain

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,18 +1,28 @@
 'use strict';
-
+var _ = require('../chain/prolodash.js');
 function calculate_elements_sum(collection, element) {
-    //在这里写入代码
-    var _ = require('../lodash/mylodash.js');
     var index = -1;
     var exist = true;
 
-    _.foreach(collection,function(n,i) {
-        if(element === collection[i] && exist) {
+    _(collection).each(function(n,i) {
+        if(element === n && exist) {
             index = i;
             exist = false;
         }
     });
     return index;
+    // //在这里写入代码
+    // var _ = require('../lodash/mylodash.js');
+    // var index = -1;
+    // var exist = true;
+    //
+    // _.foreach(collection,function(n,i) {
+    //     if(element === collection[i] && exist) {
+    //         index = i;
+    //         exist = false;
+    //     }
+    // });
+    // return index;
 }
 
 module.exports = calculate_elements_sum;
