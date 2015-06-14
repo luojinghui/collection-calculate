c0a851c4eed097657f1a8c1fe39f739f27b2a6da
luojinghui <939035243@qq.com>
2015-06-14 17:47:05 +0800

    even_group_calsulate with filter each function in prototype chain

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,28 +1,25 @@
 'use strict';
-var _ = require('../../../lodash/mylodash.js');
+var _ = require('../../../chain/prolodash.js');
 
 var even_group_calculate_average = function(collection){
-    var array = [];
-
-    array = _.filter(collection,function(n,i) {
-        return (n % 2 === 0 && i % 2 != 0);
-    });
-
     var temp = {};
     var result = [];
 
-    _.foreach(array,function(n) {
+    _(collection).filter(function(n,i) {
+        return i % 2 != 0 && n % 2 === 0;
+    }).each(function(n) {
         temp[(n + '').length] = temp[(n + '').length] || [];
         temp[(n + '').length].push(n);
     });
 
     for(var key in temp) {
         var sum = 0;
-        _.foreach(temp[key],function(n) {
+        _(temp[key]).each(function(n) {
             sum += n;
         });
         result.push(sum / temp[key].length);
     }
     return result.length === 0 ? [0] : result;
+
 };
 module.exports = even_group_calculate_average;
