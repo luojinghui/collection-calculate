86456de317758156dfe8dcafe55f60dbd94ad075
luojinghui <939035243@qq.com>
2015-06-14 15:52:59 +0800

    collection_operation_letter_mapping_1 with filter map and num_change_letter in prototype chain

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,15 +1,12 @@
 'use strict';
+var _ = require('../../chain/prolodash.js');
 
 function even_to_letter(collection) {
-    var array = [];
-    var _ = require('../../lodash/mylodash.js');
-
-    _.foreach(collection,function(n,i) {
-        if(n % 2 === 0 ) {
-            array[array.length] = String.fromCharCode(i + 97);
-        }
-    });
-    return array;
+    return _(collection).filter(function(n) {
+        return n % 2 === 0 ;
+    }).map(function(n) {
+        return _().num_change_letter(n);
+    }).value();
 }
 
 module.exports = even_to_letter;
