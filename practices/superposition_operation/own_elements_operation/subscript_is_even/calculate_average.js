440fc5f12dc85ab2d56cbecb2876352dfed6c9cf
luojinghui <939035243@qq.com>
2015-06-14 16:59:54 +0800

    calculate_average .js with filter and reduce function in prototype chain

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,27 +1,14 @@
 'use strict';
-var _ = require('../../../lodash/mylodash.js');
+var _ = require('../../../chain/prolodash.js');
 
 var calculate_average = function(collection){
-    // var sum = 0 ;
-    // var count = 0;
-    // var ave = 0;
-    //
-    // for (var i = 0 ; i < collection.length; i++) {
-    //     if (collection[i] % 2 === 0) {
-    //         sum += collection[i];
-    //         count ++;
-    //     }
-    // }
-    // ave = sum / count;
-    // return ave;
+    var even = 1;
 
-    var array = _.filter(collection,function(n,i) {
+    return _(collection).filter(function(n,i) {
         return i % 2 != 0;
-    });
-
-    var sum = _.reduce(array,function(a,b) {
+    }).reduce(function(a,b) {
+        even += 1;
         return a + b;
-    });
-    return sum / array.length;
+    }).value()/even;
 };
 module.exports = calculate_average;
