5327775f1461aba1f6489a551ea2a9d8158b86c8
luojinghui <939035243@qq.com>
2015-06-14 18:00:15 +0800

    is_exist_element with filter and is_exist function in prororype chain

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,26 +1,9 @@
 'use strict';
-var _ = require('../../../lodash/mylodash.js');
+var _ = require('../../../chain/prolodash.js');
+
 var is_exist_element = function(collection,element){
-    // for (var i = 0; i < collection.length; i++) {
-    //     if (i % 2 === 0) {
-    //         if (collection[i] === element) {
-    //             return true;
-    //         }
-    //     }
-    // }
-    //
-    // for (var j = 0; j < collection.length; j++) {
-    //     if (j % 2 === 0) {
-    //         if (collection[j] != element) {
-    //             return false;
-    //         }
-    //     }
-    // }
-
-    var array = _.filter(collection,function(n,i) {
-        return (i % 2 === 0);
-    });
-
-    return _.is_exist(array,element);
+    return _(collection).filter(function(n,i) {
+        return i % 2 === 0;
+    }).is_exist(element);
 };
 module.exports = is_exist_element;
