8b71e27729725184e5792edcc46e6f56c5e23944
luojinghui <939035243@qq.com>
2015-06-14 15:25:07 +0800

    alter median function in prototype chain

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,7 +2,7 @@
 var _ = require('../chain/prolodash.js');
 
 function compute_median(collection) {
-    return _median(collection);
+    return _(collection).median();
 }
 
 module.exports = compute_median;
