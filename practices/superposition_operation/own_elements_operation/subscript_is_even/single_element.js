7133d001bc18723d837af75a047e115c9890ef3e
luojinghui <939035243@qq.com>
2015-06-14 18:33:20 +0800

    delete old function in this question

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -18,23 +18,5 @@ var single_element = function(collection){
     }
     return result;
 
-    // var array = _.filter(collection,function(n,i) {
-    //     return i % 2 != 0;
-    // });
-    //
-    // var temp = {};
-    // var result = [];
-    //
-    // _.foreach(array,function(n) {
-    //     temp[n + 'a'] = temp[n + 'a'] || 0;
-    //     temp[n + 'a'] ++;
-    //     console.log(temp);
-    // });
-    // for(var key in temp) {
-    //     if(temp[key] === 1) {
-    //         result.push(parseInt(key));
-    //     }
-    // }
-    // return result;
 }
 module.exports = single_element;
