a1ea649bfd923de607af61406dc1c486d230f88d
luojinghui <939035243@qq.com>
2015-06-14 19:10:45 +0800

    one_add_next_myltiply_three with each function in prototype chain

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,16 +1,15 @@
 'use strict';
-var _ = require('../../lodash/mylodash.js');
+var _ = require('../../chain/prolodash.js');
 
 function one_add_next_multiply_three(collection){
     var array = [];
-    var temp = 0;
 
-    _.foreach(collection,function(n,i) {
+    _(collection).each(function(n,i) {
         if(i < collection.length - 1) {
-            temp = (n + collection[i+1]) * 3;
-            array.push(temp);
+            array.push((n + collection[i + 1]) * 3);
         }
     });
     return array;
+
 }
 module.exports = one_add_next_multiply_three;
